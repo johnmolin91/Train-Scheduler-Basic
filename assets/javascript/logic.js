aa2e01e4785064ae5b384937303c72edb82a2fb7
John Molin <john.molin91@gmail.com>
2017-12-18 15:41:27 -0500

    first commit

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCJsVQFE7WNBjvF-KmNby6bchnm2LGNir0",
+    authDomain: "train-scheduler-app-e9373.firebaseapp.com",
+    databaseURL: "https://train-scheduler-app-e9373.firebaseio.com",
+    projectId: "train-scheduler-app-e9373",
+    storageBucket: "",
+    messagingSenderId: "656788805384"
+  };
+  firebase.initializeApp(config);
